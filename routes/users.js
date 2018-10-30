9efc2af186dbbffbaadee8d0ce82782a7c3512db
boranorben <zhotkp@gmail.com>
2018-10-30 11:17:20 +0700

    fixed edit profile but still have bugs in csf

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,40 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+var User = require('../models/user');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
-// for test.
+// Edit profile for test.
 router.get('/profile', isLoggedIn, function (req, res, next) {
-  res.render('user/profile');
+  var messages = req.flash('error');
+  res.render('user/profile', {
+    csrfToken: req.csrfToken(),
+    messages: messages,
+    hasErrors: messages.length > 0,
+    title: 'User Profile'
+   });
 });
+
+router.post('/profile', function(req, res, next) {
+  var updUser = new User(req.session.user ? req.session.user : {});
+  updUser.firstName = req.body.firstName;
+  updUser.lastName = req.body.lastName;
+  updUser.address = req.body.address;
+  updUser.city = req.body.city;
+  updUser.state = req.body.state;
+  updUser.zip = req.body.zip;
+  User.update({ _id: req.session.user.id }, updUser, function(err, done) {
+      if (err) {
+          return console.log("err");
+      } else {
+          console.log("pass");
+          req.session.user = updUser;
+          res.render('user/profile');
+      }
+  })
+})
 
 router.get('/logout', isLoggedIn, function (req, res, next) {
   req.logout();

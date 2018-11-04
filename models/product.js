91bda9f610ac6b9c5543bfa37e0a35c0870c1b85
boranorben <zhotkp@gmail.com>
2018-11-04 20:34:47 +0700

    modified product model (again)

--- models/product.js
+++ models/product.js
@@ -7,6 +7,7 @@ var schema = new Schema({
     type: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
+    detail: { type: String, required: true },
     price: { type: Number, required: true },
     color: { type: String, required: true },
     size: { type: String, required: true },

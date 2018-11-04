ee787c49b760bbcda233c2c2d281aa1c91e1ef97
boranorben <zhotkp@gmail.com>
2018-11-04 16:14:24 +0700

    modified product model and update product page

--- models/product.js
+++ models/product.js
@@ -1,19 +1,29 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
+// var schema = new Schema({
+//     title: { type: String, required: true },
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     // color: { type: String, size: [{
+//     //     type: String,
+//     //     qty: Number
+//     // }] },
+//     imagePath_1: { type: String, required: true },
+//     imagePath_2: { type: String, required: true },
+//     imagePath_3: { type: String, required: true },
+//     imagePath_4: { type: String, required: true },
+//     imagePath_5: { type: String, required: true }
+// })
+
 var schema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    // color: { type: String, size: [{
-    //     type: String,
-    //     qty: Number
-    // }] },
-    imagePath_1: { type: String, required: true },
-    imagePath_2: { type: String, required: true },
-    imagePath_3: { type: String, required: true },
-    imagePath_4: { type: String, required: true },
-    imagePath_5: { type: String, required: true }
+    department: { type: String },
+    catalog: { type: String },
+    title: { type: String },
+    description: { type: String },
+    price: { type: Number },
+    color: { type: Object },
+    size: { type: Object }
 })
 
 module.exports = mongoose.model('Product', schema);
